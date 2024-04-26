7174d73807c6494bad716c81e0597c8ecef600df
Pranoy Kundu <pranoy1998k@gmail.com>
2024-04-26 11:38:29 +0530

    refactor read_lines

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
+use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
-use std::env;
 
 fn main() {
     let line_count = 10;
@@ -9,20 +8,22 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filepath = &args[1];
 
-    if let Ok(lines) = read_lines(filepath) {
-        for line in lines.take(line_count as usize) {
-            if let Ok(ip) = line {
-                println!("{}", ip);
-            } else {
-                println!("### Error while reading line from file. ###");
-                break;
-            }
-        }
-    }
+    read_lines(filepath, line_count);
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
-where P: AsRef<Path>,{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn read_lines(filename: &str, line_count: u32) {
+    let file = File::open(filename).unwrap_or_else(|e| {
+        println!("rs-head: cannot open '{}' for reading: {}", filename, e);
+        std::process::exit(1);
+    });
+    io::BufReader::new(file)
+        .lines()
+        .take(line_count as usize)
+        .for_each(|line| match line {
+            Ok(l) => println!("{}", l),
+            Err(e) => {
+                println!("rs-head: error reading line: {}", e);
+                std::process::exit(1);
+            }
+        });
 }
